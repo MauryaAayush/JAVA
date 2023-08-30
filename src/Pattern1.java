8aefb9bb516600a53b42f37361522fc48e255c15
Aayush <maayush235@gmail.com>
2023-08-30 22:37:26 +0530

    30/08/2023

--- src/Pattern1.java
+++ src/Pattern1.java
@@ -1,6 +1,12 @@
 public class Pattern1 {
     public static void main(String[] args)
     {
-        System.out.println("****** ");
+        System.out.println(" - - - - - -  ");
+        System.out.println("|\t\t\t|");
+        System.out.println("|\t\t\t|");
+        System.out.println("|\tAayush\t|");
+        System.out.println("|\t\t\t|");
+        System.out.println("|\t\t\t|");
+        System.out.println(" - - - - - - ");
     }
 }

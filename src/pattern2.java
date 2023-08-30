535e6587377c1a3d3f82e2f08044038ff690ebeb
Aayush <maayush235@gmail.com>
2023-08-30 23:54:58 +0530

    30/08/2023

--- src/pattern2.java
+++ src/pattern2.java
@@ -2,9 +2,15 @@ public class pattern2
 {
     public static void main (String [] args)
     {
-        System.out.println("*");
-        System.out.println("*");
-        System.out.println("*");
-        System.out.println("*");
+        System.out.println(" - - - - ");
+        System.out.println("|\t\t|");
+        System.out.println("A\t\t|");
+        System.out.println("a\t\t|");
+        System.out.println("y\t\t|");
+        System.out.println("u\t\t|");
+        System.out.println("s\t\t|");
+        System.out.println("h\t\t|");
+        System.out.println("|\t\t|");
+        System.out.println(" - - - - ");
     }
 }
---
+++ src/pattern5.java
@@ -0,0 +1,17 @@
+public class pattern5 {
+ public static void main(String [] args)
+    {
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*\t\t\t\t\t\t* *");
+        System.out.println("*     * *\t\t\t  *  \t*");
+        System.out.println("*    *    *         *");
+        System.out.println("*   *       *     *");
+        System.out.println("*  *          * *");
+        System.out.println("*");
+
+
+
+    }
+}
